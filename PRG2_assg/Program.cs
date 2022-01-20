ada98c94df3b3f978face31d21791ba614af0fcc
AngMingZe <angmingze21@gmail.com>
2022-01-20 16:46:33 +0800

    Add project files.

---
+++ PRG2_assg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRG2_assg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
