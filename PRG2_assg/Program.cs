4f0c32a25852988b69157f2655c87dad97f4900b
AngMingZe <angmingze21@gmail.com>
2022-01-21 15:14:18 +0800

    Some updates to the main program code

--- PRG2_assg/Program.cs
+++ PRG2_assg/Program.cs
@@ -7,6 +7,42 @@ namespace PRG2_assg
         static void Main(string[] args)
         {
             //Last updated: 21/1/2022
+            while (true)
+            {
+                Menu();
+                Console.Write("Enter option: ");
+                int op = Convert.ToInt32(Console.ReadLine());
+                if (op == 1)
+                {
+                    //do op 1 things
+                }
+                if (op == 2)
+                {
+                    //do op 2 things
+                }
+                if (op == 3)
+                {
+                    //do op 3 things 
+                }
+                if (op == 0)
+                {
+                    Console.WriteLine("Thanks for using our system");
+                    break;
+                }
+            }
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("[1] General");
+            Console.WriteLine("[2] Screening");
+            Console.WriteLine("[3] Order");
+            Console.WriteLine("[0] Exit");
+        }
+
+        public static void LoadMandCData()
+        {
+            //Load data
         }
     }
 }
