a791e39f49c3fd5f76296892fc77962c99157b5e
Guojun <s10227742@connect.np.edu.sg>
2022-01-21 14:05:54 +0800

    Update Cinema, Movie, Screening

--- PRG2_assg/Cinema.cs
+++ PRG2_assg/Cinema.cs
@@ -7,5 +7,26 @@ namespace PRG2_assg
     class Cinema
     {
         //Last updated: 21/1/2022
+
+        //Attr
+        public string Name { get; set; }
+
+        public int HallNo { get; set; }
+
+        public int Capacity { get; set; }
+
+        //Constructor
+        public Cinema() { }
+
+        public Cinema(string n, int hn, int c)
+        {
+            Name = n;
+            HallNo = hn;
+            Capacity = c;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + "HallNo: " + HallNo + "Capacity: " + Capacity;
+        }
     }
 }
